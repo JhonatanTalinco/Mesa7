2039d381f6f62db2b8280a76ec9602e0174a9539
Jhonatan Talinco <jhonatantalincossilva@gmail.com>
2022-08-16 20:33:24 -0500

    Mesa7

---
+++ mesa7.js
@@ -0,0 +1,19 @@
+//O que cada Expressão retorna?
+
+console.log(!true); // Retorna false, tendo em vista que o sinal de exclamação significa uma negação
+console.log(!false); // Retorna true
+// console.log(!!false true); // o código dá erro
+console.log(!1); // Retorna false
+console.log(!!1); // Retorna true
+console.log(!0); // Retorna true
+console.log(!!0); // Retorna false
+console.log(!!""); // Retorna false
+let x = 5;
+console.log(x); // Retorna 5
+let y = 9;
+
+console.log(x < 10 && x!==5); // Retorna false
+
+console.log ( x > 9 || x===5); // Retorna true
+
+console.log(!x===y); // Retorna false
